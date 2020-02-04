e09f942adb0bf11b087a0ee2dd78ee73eac72d68
hyeyeong1011 <52442799+hyeyeong1011@users.noreply.github.com>
2020-02-04 15:52:18 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,43 @@ $(function(){
         loopBottom: true,
         sectionsColor: ['#eedcd5', '#eedcd5', '#eedcd5', '#eedcd5', '#eedcd5'],
         anchors: ['first', 'second', 'third', 'fourth', 'last'],
+        'afterLoad': function(anchorLink, index){
+            
+            if(index == 1){
+
+                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
+                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
+                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
+                $(".skill-more").removeClass('tilt-in-top-4');
+
+            }else if(index == 2){
+
+                $(".skill-item:nth-child(1)").addClass('tilt-in-top-1');
+                $(".skill-item:nth-child(2)").addClass('tilt-in-top-2');
+                $(".skill-item:nth-child(3)").addClass('tilt-in-top-3');
+                $(".skill-more").addClass('tilt-in-top-4');
+
+            }else if(index == 3){
+
+                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
+                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
+                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
+                $(".skill-more").removeClass('tilt-in-top-4');
+
+            }else if(index == 4){
+
+                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
+                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
+                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
+                $(".skill-more").removeClass('tilt-in-top-4');
+
+            }else{
+
+
+
+            }
+
+        },
     });
 
 
