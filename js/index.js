b4a3e21a7d4b858ccb8bb0efccaf860b76ac3c06
hyeyeong1011 <52442799+hyeyeong1011@users.noreply.github.com>
2020-02-20 14:38:46 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,41 +5,23 @@ $(function(){
         scrollHorizontally: true,
         navigation: true,
         loopBottom: true,
-        sectionsColor: ['#eedcd5', '#eedcd5', '#eedcd5', '#eedcd5', '#eedcd5'],
-        anchors: ['first', 'second', 'third', 'fourth', 'last'],
+        sectionsColor: ['#eedcd5', '#eedcd5', '#eedcd5'],
+        anchors: ['first', 'second', 'third'],
         'afterLoad': function(anchorLink, index){
             
-            if(index == 1){
+            if(index == 2){
 
-                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
-                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
-                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
-                $(".skill-more").removeClass('tilt-in-top-4');
+                $(".skill-item:nth-child(1)").addClass('flip-bottom-1');
+                $(".skill-item:nth-child(2)").addClass('flip-bottom-2');
+                $(".skill-item:nth-child(3)").addClass('flip-bottom-3');
+                $(".skill-more").addClass('flip-bottom-4');
 
-            }else if(index == 2){
+            }else if(index != 2){
 
-                $(".skill-item:nth-child(1)").addClass('tilt-in-top-1');
-                $(".skill-item:nth-child(2)").addClass('tilt-in-top-2');
-                $(".skill-item:nth-child(3)").addClass('tilt-in-top-3');
-                $(".skill-more").addClass('tilt-in-top-4');
-
-            }else if(index == 3){
-
-                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
-                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
-                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
-                $(".skill-more").removeClass('tilt-in-top-4');
-
-            }else if(index == 4){
-
-                $(".skill-item:nth-child(1)").removeClass('tilt-in-top-1');
-                $(".skill-item:nth-child(2)").removeClass('tilt-in-top-2');
-                $(".skill-item:nth-child(3)").removeClass('tilt-in-top-3');
-                $(".skill-more").removeClass('tilt-in-top-4');
-
-            }else{
-
-
+                $(".skill-item:nth-child(1)").removeClass('flip-bottom-1');
+                $(".skill-item:nth-child(2)").removeClass('flip-bottom-2');
+                $(".skill-item:nth-child(3)").removeClass('flip-bottom-3');
+                $(".skill-more").removeClass('flip-bottom-4');
 
             }
 
@@ -47,13 +29,5 @@ $(function(){
     });
 
 
-    // 아이콘 깜박거리기
-    function blink(){
-        $('.box i').delay(100).fadeTo(100,0.5).delay(100).fadeTo(100,1, blink);
-    }
-
-    $(document).ready(function() {
-        blink();
-    });
 
 });
